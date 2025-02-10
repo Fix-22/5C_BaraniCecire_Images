20486ea3d8b0baddf11c4df0feb7ef4e2f1b77d4
Simone Cecire <ceciresimone@itis-molinari.eu>
2025-02-10 10:49:09 +0100

    librerie

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+const mysql = require('mysql2');
+const multer  = require('multer');
+
