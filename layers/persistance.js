b3f3b20ebcb3436aa9a73cd6d90c38ce359350f0
Simone Cecire <ceciresimone@itis-molinari.eu>
2025-02-11 12:08:05 +0100

    prepearazione layer persistenza

--- layers/persistance.js
+++ layers/persistance.js
@@ -1,3 +1,47 @@
-module.exports.persistance = () => {
-    
+const path = require('path');
+const mysql = require('mysql2');
+const multer  = require('multer');
+
+module.exports = function generatePersistance(conf) {
+    const connection = mysql.createConnection(conf);
+
+    let storage = multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, path.join(__dirname, "files"));
+        },
+        filename: function (req, file, callback) {
+            callback(null, file.originalname);
+        }
+    });
+    const upload = multer({storage: storage}).single('file');
+
+    const executeQuery = (sql) => {
+        return new Promise((resolve, reject) => {      
+            connection.query(sql, function (err, result) {
+                if (err) {
+                    console.error(err);
+                    reject();
+                }
+                resolve(result);         
+            });
+        })
+    };
+
+    executeQuery(`
+        CREATE TABLE IF NOT EXISTS image
+            (id INT PRIMARY KEY AUTO_INCREMENT, 
+                url varchar(255) NOT NULL)
+    `);
+
+    return {
+        insertImage: async (image) => {
+
+        },
+        selectAllImages: async () => {
+
+        },
+        deleteImage: async (id) => {
+             
+        }
+    };
 };
--- server.js
+++ server.js
@@ -3,43 +3,21 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
-const mysql = require('mysql2');
-const multer  = require('multer');
+const generatePersistance = require("./layers/persistance.js");
 
 const conf = JSON.parse(fs.readFileSync('conf.json'));
 conf.ssl.ca = fs.readFileSync(__dirname + "/ca.pem");
-const connection = mysql.createConnection(conf);
 
-const executeQuery = (sql) => {
-    return new Promise((resolve, reject) => {      
-        connection.query(sql, function (err, result) {
-            if (err) {
-                console.error(err);
-                reject();     
-            }
-            resolve(result);     
-        });
-    })
-};
-
-let storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, path.join(__dirname, "files"));
-    },
-    filename: function (req, file, callback) {
-        callback(null, file.originalname);
-    }
-});
+const persistance = generatePersistance(conf);
 
 const app = express();
-const upload = multer({storage: storage}).single('file');
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 
 app.post('/add', (req, res) => {
-
+    
 });
 
 app.get('/get', (req, res) => {
